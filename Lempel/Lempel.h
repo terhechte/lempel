3c29cfa43bdd42db5e61d1d9c0d5d6a80be97a9e
Benedikt Terhechte <terhechte@gmail.com>
2015-12-17 21:45:44 +0100

    Initial Commit

---
+++ Lempel/Lempel.h
@@ -0,0 +1,19 @@
+//
+//  Lempel.h
+//  Lempel
+//
+//  Created by Benedikt Terhechte on 17/12/15.
+//  Copyright © 2015 Benedikt Terhechte. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Lempel.
+FOUNDATION_EXPORT double LempelVersionNumber;
+
+//! Project version string for Lempel.
+FOUNDATION_EXPORT const unsigned char LempelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lempel/PublicHeader.h>
+
+
